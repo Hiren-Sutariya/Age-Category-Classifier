057180fa6e0fc4dc37ef1d65c4faaa82d2eded68
Sutariya-Hiren <sutariyahiren14@gmail.com>
2025-12-18 00:18:34 +0530

    age calculate

---
+++ age.py
@@ -0,0 +1,11 @@
+age = 18
+
+
+if (age < 13):
+    print("You'r a child")
+elif (age < 20):
+    print("You'r a Teenager")
+elif (age < 60):
+    print("You'r a Adult")
+else :
+    print("You'r a senior")
